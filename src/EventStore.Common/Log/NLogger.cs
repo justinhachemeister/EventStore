fbcb06f36575e38bb925cdfbb42b1b9a15210773
shaan1337 <sniper111@gmail.com>
2018-07-25 14:49:58 +0400

    Miscellaneous fixes

--- src/EventStore.Common/Log/NLogger.cs
+++ src/EventStore.Common/Log/NLogger.cs
@@ -70,7 +70,6 @@ namespace EventStore.Common.Log
         public void Trace(string format, params object[] args)
         {
             _logger.Trace(format, args);
-            
         }
 
         public void FatalException(Exception exc, string format, params object[] args)
